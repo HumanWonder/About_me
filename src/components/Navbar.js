5c641dbe0da848311da2ead0e200a334abba577b
afouquem <axellefouq@hotmail.fr>
2024-12-13 08:54:01 +0100

    gestion affichage boutons navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,29 +3,43 @@ import { Link, useLocation } from 'react-router-dom'; // Import des composants d
 import "../styles/App.css";
 
 const Navbar = () => {
-    const location = useLocation(); // Permet d'accéder à l'URL actuelle
-  
-    const tabs = [
-      { id: "home", label: "Home", path: "/" },
-      { id: "projects", label: "Projects", path: "/projects" },
-      { id: "hobbies", label: "Hobbies", path: "/hobbies" },
-      // { id: "game", label: "Game", path: "/game" },
-    ];
-  
-    return (
-      <div className="navbar-buttons">
-        {tabs.map((tab) => (
-          <Link 
-            key={tab.id} 
-            to={tab.path} 
-            className={location.pathname === tab.path ? "active" : ""} // Applique 'active' si le path correspond
-          >
-            
-            {tab.label}
-          </Link>
-        ))}
-      </div>
-    );
-  };
+  const location = useLocation(); // Permet d'accéder à l'URL actuelle
+
+  const tabs = [
+    { id: "home", label: "Home", path: "/" },
+    { id: "projects", label: "Projects", path: "/projects" },
+    { id: "hobbies", label: "Hobbies", path: "/hobbies" },
+    // { id: "game", label: "Game", path: "/game" }, Pas affiché mais considéré comme une page active
+  ];
+
+  // Gestion des onglets à afficher
+  const filteredTabs = tabs
+    .filter((tab) => tab.id !== "game") // Exclut toujours "Game" de la navbar
+    .map((tab) => {
+      if (tab.id === "projects" && location.pathname === "/game") {
+        // Renomme "Projects" en "Retour" lorsque "Game" est actif
+        return { ...tab, label: "Back" };
+      }
+      return tab;
+    });
+
+  // Exclut l'onglet actif
+  const displayedTabs = filteredTabs.filter((tab) => tab.path !== location.pathname);
+
+  return (
+    <div className="navbar-buttons">
+      {displayedTabs.map((tab) => (
+        <Link
+          key={tab.id}
+          to={tab.path}
+          className={location.pathname === tab.path ? "active" : ""}
+        >
+          {tab.label}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
 
   export default Navbar;
